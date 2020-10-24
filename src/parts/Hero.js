7c5a0600f23e20571c7ee121a29a41d7e49fed12
R-Technology <43551882+Farizal97@users.noreply.github.com>
2020-10-25 05:15:50 +0700

    hero

---
+++ src/parts/Hero.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+
+import imageHero from 'assets/images/img-hero.jpg'
+
+export default function Hero(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
